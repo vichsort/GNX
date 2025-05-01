b2d6f31bf7b126bcf02bb85f8e57e06edc7d0b9c
PietroMatheus <140650026+PietroMatheus@users.noreply.github.com>
2025-05-01 20:06:42 -0300

    Update apireq.py

--- apireq.py
+++ apireq.py
@@ -1,18 +1,19 @@
 from flask import Flask, request
 import requests
 import uuid
-import psyco
+import db
 from config import apikey
 
-url = f'http://www.omdbapi.com/?apikey={apikey}'
-listreq = []
+db.generator()
 
+url = f'https://www.omdbapi.com/{apikey}&'
 app = Flask(__name__)
+listreq = []
 
 @app.route("/request/<caller>", methods=["POST"])
 def sender(caller):
     dados_receba = request.get_json()
-    informacao = dados_receba['info']
+    informacao = dados_receba.get('info')
 
     listreq.append({
         'ticket': str(uuid.uuid4()),
@@ -20,38 +21,33 @@ def sender(caller):
         'title': informacao
     })
 
-    if (caller == "title" or caller == "id"):
-        call(informacao, caller)
-
-    return caller
+    if caller in ("title", "id"):
+        return call(informacao, caller)
+    return {'error': 'Caller inválido'}, 400
 
 @app.route("/call", methods=["GET"])
-def call(param, caller):
-    print(param + " recebido com sucesso, tipo: " + caller)
-    if (caller == "title"):
-        caller = "t="
+def call(param=None, caller=None):
+    if not param or not caller:
+        caller = request.args.get('type')
+        param = request.args.get('info')
+
+    key = 't' if caller == 'title' else 'i'
+    full_url = f"{url}{key}={param}"
+    print(f"Chamando OMDb: {full_url}")
+
+    resp = requests.get(full_url)
+    if resp.status_code == 200:
+        data = resp.json()
+        if data.get('Response') == 'True':
+            print("Requisição bem-sucedida:", data)
+            db.catcher(data)
+            return data, 200
+        else:
+            print(f"OMDb erro: {data.get('Error')}")
+            return {'error': data.get('Error')}, 502
     else:
-        caller = "i="
-    var_resposta = requests.get(url + caller + param)
-    if (var_resposta.status_code == 200):
-        print("tudo certo com a requisição.")
-        print(var_resposta.text)
-        psyco.catcher(var_resposta);
-    else:
-        print('erro! ' + var_resposta.status_code)
+        print(f"HTTP error {resp.status_code}")
+        return {'error': 'HTTP ' + str(resp.status_code)}, resp.status_code
 
-
-
-# Search by title
-
-# https://www.omdbapi.com/?apikey=8193de3c&t=the batman
-
-
-# Search by ID
-
-# https://www.omdbapi.com/?apikey=8193de3c&i=tt1877830
-
-
-# Search by search parameter
-
-# https://www.omdbapi.com/?apikey=8193de3c&s=jaws&type=movie
+if __name__ == '__main__':
+    app.run(debug=True)
