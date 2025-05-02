cffeec22632fd00c4afa35227cb6c23b232bd1ca
Vitor Mignoni <vichsort@proton.me>
2025-05-01 23:04:48 -0300

    comm apireq.py

--- apireq.py
+++ apireq.py
@@ -1,53 +1,57 @@
 from flask import Flask, request
 import requests
-import uuid
 import db
 from config import apikey
 
-db.generator()
+db.generator() # chama o gerador do banco
 
-url = f'https://www.omdbapi.com/{apikey}&'
+url = f'https://www.omdbapi.com/?apikey={apikey}&'
 app = Flask(__name__)
-listreq = []
 
 @app.route("/request/<caller>", methods=["POST"])
 def sender(caller):
+    # "CALLER" é responsável por chamar e
+    # adquirir as informações passadas para
+    # enviar com uma outra função estes
+    # aspectos pro banco de dados. 
+    # dentro dela pegamos o JSON e o tipo
+    # de requisição (titulo ou id) que esta
+    # sendo realizada. 
+
     dados_receba = request.get_json()
-    informacao = dados_receba.get('info')
+    informacao = dados_receba.get('info') # só o que importa
 
-    listreq.append({
-        'ticket': str(uuid.uuid4()),
-        'type': caller,
-        'title': informacao
-    })
-
-    if caller in ("title", "id"):
+    if caller in ("title", "id"): 
+        # se for um dos dois aceitos vai lançar pro bd
         return call(informacao, caller)
     return {'error': 'Caller inválido'}, 400
 
 @app.route("/call", methods=["GET"])
 def call(param=None, caller=None):
+    # "CALL", realiza então a chamada/ponte
+    # entre o banco de dados e o codigo em
+    # flask. Pra nao dar erro por padrão (algo
+    # que me estressou muito escrevendo essa
+    # parte) define já como None.
+ 
     if not param or not caller:
+        # só pra garantir...
         caller = request.args.get('type')
         param = request.args.get('info')
 
-    key = 't' if caller == 'title' else 'i'
-    full_url = f"{url}{key}={param}"
-    print(f"Chamando OMDb: {full_url}")
+    key = 't' if caller == 'title' else 'i'     # operador ternario ridiculo (js >>>)
+    full_url = f"{url}{key}={param}"            # imagino que isso não seja seguro
 
-    resp = requests.get(full_url)
+    resp = requests.get(full_url)               # realiza o GET na API
     if resp.status_code == 200:
         data = resp.json()
         if data.get('Response') == 'True':
-            print("Requisição bem-sucedida:", data)
-            db.catcher(data)
+            print("Requisição bem-sucedida:", data) # teste por agora
+            db.catcher(data) # aqui a magica acontece
             return data, 200
         else:
-            print(f"OMDb erro: {data.get('Error')}")
+            print(f"OMDb erro: {data.get('Error')}") # quando o erro é no omdb
             return {'error': data.get('Error')}, 502
     else:
-        print(f"HTTP error {resp.status_code}")
+        print(f"req erro: {resp.status_code}") # quando o erro é meu
         return {'error': 'HTTP ' + str(resp.status_code)}, resp.status_code
-
-if __name__ == '__main__':
-    app.run(debug=True)
