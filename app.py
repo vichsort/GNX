f7a43b128501ece1e19d95516fa3ac6a41744bcf
Vitor Mignoni <vichsort@proton.me>
2025-05-02 16:15:03 -0300

    final

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import requests
+from config import secret_key
 
 app = Flask(__name__)
+app.secret_key = secret_key
 
 # Endereço onde o apireq está rodando
 APIREQ_BASE = 'http://localhost:5000'
@@ -20,7 +22,6 @@ def red_home():
 
 @app.route('/buscar', methods=['POST'])
 def buscar():
-    # Extrai dados do form
     caller = request.form.get('caller')   # 'title' ou 'id'
     info = request.form.get('info')       # o texto que o usuário digitou
 
@@ -28,21 +29,19 @@ def buscar():
         flash('Você precisa preencher o campo de busca.', 'warning')
         return redirect(url_for('home'))
 
-    # Monta e dispara a requisição na APi
-    url = f"{APIREQ_BASE}/request/{caller}"
-    resp = requests.post(url, json={'info': info})
+    url = f"{APIREQ_BASE}/request/{caller}" # seta a url pra mandar a requisição
+    resp = requests.post(url, json={'info': info}) # passa os dados em formato json, simulando um usuário por client
 
     if resp.ok:
-        # Se tudo deu certo, traz o JSON de resposta
-        # pra evitar erros maiores que podem dar
-        # dentro da API
         resultado = resp.json()
         return render_template('index.html', result=resultado)
-    else:
-        # so pra esclarecer, eu esqueci de colocar um '?apikey='
-        # uma hora e dai deu um erro de alerta que fez eu cagar
-        # nas calças. Dai eu coloquei isso aqui que aprendi com
-        # o gustavo guanabara (genio)
-        flash(f"Erro na busca (HTTP {resp.status_code})", 'danger')
+    else: # alertas periódicos
+        if resp.status_code == 502:
+            flash('Filme não encontrado no banco de dados IMDb.', 'warning')
+        else:
+            flash(f"Erro na busca (HTTP {resp.status_code})", 'danger')
         return redirect(url_for('home'))
 
+
+if __name__ == "__main__":
+    app.run(debug=False, port=5001)
